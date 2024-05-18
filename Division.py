51ce80a046ed70f871e6c7171618e9fd29c0ee89
santiago.vasco <santiagovascolz@gmail.com>
2024-05-18 00:03:18 -0500

    gitignore

---
+++ Division.py
@@ -0,0 +1,7 @@
+def division(num1, num2):
+    if num2 == 0:
+        print("ERROR: Division by zero")
+    else:
+        result = num1 / num2
+        print(f'{num1} / {num2} is equal to {result}')
+        return result
